0b0f403886957acba92f1a74d68a7d2a3c359f56
zhouyy1 <bluenoah1991@outlook.com>
2026-01-27 18:19:32 +0800

    chore: configure terser minification for web client build

--- clients/web/vite.config.ts
+++ clients/web/vite.config.ts
@@ -5,6 +5,14 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: '/',
-  build: {outDir: '../../dist/clients/web', emptyOutDir: true},
+  build: {
+    outDir: '../../dist/clients/web',
+    emptyOutDir: true,
+    minify: 'terser',
+    terserOptions: {
+      compress: {drop_console: false, drop_debugger: true},
+      mangle: {properties: {regex: /^_private/}},
+    },
+  },
   server: {proxy: {'/api': 'http://localhost:5281'}},
 });
