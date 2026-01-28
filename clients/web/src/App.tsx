ddbc5b903ecabe529c5f4e469c12ff2282f6f269
zhouyy1 <bluenoah1991@outlook.com>
2026-01-28 17:22:35 +0800

    feat: set document title to project name from config

--- clients/web/src/App.tsx
+++ clients/web/src/App.tsx
@@ -46,6 +46,10 @@ export default function App() {
     }
   }, [currentModel?.provider, getSessions]);
 
+  useEffect(() => {
+    document.title = config?.projectName || 'OmniContext CLI';
+  }, [config?.projectName]);
+
   return (
     <div className='flex h-screen bg-vscode-bg text-vscode-text text-sm overflow-hidden'>
       <div className='flex-1 flex flex-col min-w-0'>
